fc9948f06d629160eacab77c039c0fe059d87c56
Louis Borres <lborresbusiness@gmail.com>
2025-12-30 04:01:34 +0800

    docs: complete basic example with logging

--- examples/fiber-pgx-basic/main.go
+++ examples/fiber-pgx-basic/main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"context"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/lborres/kuta"
 	fiberadapter "github.com/lborres/kuta/adapters/fiber"
 	pgxadapter "github.com/lborres/kuta/adapters/pgx"
 )
+
+func logFormat() string {
+	format := []string{
+		// Timestamp & Request ID
+		"${time}|${requestid}",
+
+		// Response metadata
+		"${status}|${latency}",
+
+		// Client info
+		"${ip}:${port}",
+
+		// Transfer size
+		"${bytesReceived}|${bytesSent}",
+
+		// Request details
+		"${method}|${path}|${queryParams}",
+
+		// Request body & errors
+		"${body}|${error}",
+	}
+	return strings.Join(format, "|") + "\n"
+}
 
 func main() {
 	ctx := context.Background()
@@ -23,8 +48,20 @@ func main() {
 
 	app := fiber.New()
 
+	// Fiber's Register Fiber's built-in Logger
+	// for debugging purposes only
+	// disregard this block
+	app.Use(logger.New(logger.Config{
+		Format:     logFormat(),
+		TimeFormat: "2006/01/02 15:04:05",
+		TimeZone:   "Local",
+	}))
+
 	_, err = kuta.New(kuta.Config{
-		Secret:        "mysupersecretsecret", // Provide this with env variables
+		// WARN: Demonstration purposes only
+		// provide your secret in a more secure way such as environment variables
+		Secret: "mysupersecretsecret",
+
 		Database:      pgxadapter.New(pool),
 		HTTP:          fiberadapter.New(app),
 		SessionConfig: &kuta.SessionConfig{MaxAge: 24 * time.Hour},
